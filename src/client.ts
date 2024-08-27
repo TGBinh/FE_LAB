4be32ae102bbab189242bf08b1073c4e23a88b33
TGBinh <truongiabinh1231@gmail.com>
2024-08-27 16:24:53 +0700

    update part 2

--- src/client.ts
+++ src/client.ts
@@ -2,13 +2,19 @@ import { createPublicClient, createWalletClient, custom, http } from "viem";
 import { mainnet, sepolia, bsc } from "viem/chains";
 // import "viem/window"
 
+let transport = null; 
+if (typeof window !== 'undefined' && window.ethereum) {
+  transport = custom(window.ethereum);
+} else {
+  transport = http()
+}
+
 export const publicClient = createPublicClient({
-  chain: sepolia,        
+  chain: sepolia,        // <--- This is the chain that the client will interact with
   transport: http(),
 });
 
-
 export const walletClient = createWalletClient({
   chain: sepolia,
-  transport: custom(window.ethereum),
+  transport: transport!,
 });
